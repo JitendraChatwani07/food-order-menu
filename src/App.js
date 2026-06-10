2bc7ef47877717234c62bb2afa668551012ebc65
JitendraChatwani07 <chatwanijitu1@gmail.com>
2026-06-10 16:57:50 +0530

    Create src/App.js main component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Food Order Menu</h1>
+        <p>Your favorite food ordering application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
